ce6bab0d4522dd4ec16f99fc23b68d27e8215665
JustAnotherSoloDev <110450952+JustAnotherSoloDev@users.noreply.github.com>
2024-03-19 22:03:36 +0000

    Deploying to pages from @ JustAnotherSoloDev/shre-me@21ed61b139677d1bf136f3a457856ece1cce53b1 🚀

--- assets/index-Dj18hlzU.js
+++
@@ -1,6 +0,0 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();function u(){let n=document.getElementById("file").value,r=document.createElement("a");r.href="/"+n,r.innerText=n,document.body.appendChild(r)}document.querySelector("#app").innerHTML=`
-  <div>
-  <input id="file" type="text"/>
-  <button id="button">Create Link</button>
-  </div>
-`;document.getElementById("button").addEventListener("click",u);
