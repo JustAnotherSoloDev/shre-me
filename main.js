b895438194c9d7db5215cf031b49b270013187a9
solo <devendrarawat76@gmail.com>
2024-03-19 22:56:34 +0100

    Initial

---
+++ main.js
@@ -0,0 +1,18 @@
+import "./style.css";
+
+function create() {
+  let file = document.getElementById("file").value;
+  let link = document.createElement("a");
+  link.href = "/" + file;
+  link.innerText = file;
+  document.body.appendChild(link);
+}
+
+document.querySelector("#app").innerHTML = `
+  <div>
+  <input id="file" type="text"/>
+  <button id="button">Create Link</button>
+  </div>
+`;
+
+document.getElementById("button").addEventListener("click",create);
